7c909c1fd0fa1ddba0b4f49cdc628741ab8d0518
RobertJanjanin <55134583+RobertJanjanin@users.noreply.github.com>
2020-12-01 10:00:46 +0100

    Add files via upload

---
+++ zad3/zad3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bezPDV;
+            int PDV;
+            Console.WriteLine("Unesi cijenu bez PDVa: ");
+            bezPDV =Convert.ToInt32( Console.ReadLine());
+            Console.WriteLine(bezPDV + bezPDV * 0.25);
+            Console.WriteLine("Unesi cijenu s PDVom: ");
+            PDV = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(PDV * 0.80);
+            Console.ReadKey();
+        }
+    }
+}
